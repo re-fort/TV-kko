b713eefb500ec214b0db000d1c934c7783c884b5
re-fort <jeffpko@gmail.com>
2016-06-11 22:54:07 +0900

    Implement #1

---
+++ renderer/util/moment.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const m = require('moment')
+
+const moment = {
+  getCurrentDate() {
+    return m()
+  },
+
+  getTimerDate(currentDate, hour) {
+    let timerDate = this.getCurrentDate()
+    timerDate.hour(hour).minute(0).second(0)
+
+    if (timerDate.isBefore(currentDate)) timerDate = timerDate.add(1, 'days')
+    
+    return timerDate
+  },
+  
+  getDiff(fromDate, toDate) {
+    return toDate.diff(fromDate, 'milliseconds')
+  }
+}
+
+module.exports = moment
